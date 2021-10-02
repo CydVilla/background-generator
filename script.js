a383ab5be82fa1f958939daf6c1eb27dc24519f2
cydvilla <villacv@gmail.com>
2021-10-02 14:02:30 -0400

    added event listeners

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const css = document.queryCommandValue("h3");
 const color1 = document.querySelector(".color1")
 const color2 = document.querySelector(".color2")
 
-console.log(css)
-console.log(color1)
-console.log(color2)
+
+color1.addEventListener("input", function(){
+	console.log(color1.value);
+})
+
+color2.addEventListener("input", function(){
+	console.log(color2.value)
+})
